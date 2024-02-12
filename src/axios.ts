312d03921feeb0d1e334f92de6081020c01575ce
paceCodenp <bibash@pacecode.com.np>
2024-02-12 18:39:37 +0545

    update the env

--- src/axios.ts
+++ src/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5001";
+axios.defaults.baseURL =  process.env.VUE_APP_BACKEND || "http://localhost:5001";
 
 axios.defaults.headers.common["Authorization"] =
 	"Bearer " + localStorage.getItem("token");
